25cd84615acbd16797010cf7738dda7fc2a8dd8b
Ksenia Dedik <kseniya.dedik@gmail.com>
2024-03-28 15:20:16 +0100

    Routing setup

---
+++ src/Pages/HomePage/Home.tsx
@@ -0,0 +1,8 @@
+import { useRef } from 'react';
+import styles from './Home.module.css';
+
+export const Home = () => {
+  const ref = useRef<null | HTMLDivElement>(null);
+
+  return <div>Hiiiiii</div>;
+};
---
+++ src/Pages/NoPage/NoPage.tsx
@@ -0,0 +1,18 @@
+import styles from './NoPage.module.css';
+
+export const NoPage = () => {
+  return (
+    <div className={styles.NoPage}>
+      <p className={styles.Title}>404</p>
+      <p className={styles.NoPageText}>
+        Sorry. the content you&apos;re looking for doesn&apos;t exist. Either it was removed, or you
+        mistyped the link.
+      </p>
+      <a href="/">
+        <button type="submit" className={styles.Button}>
+          <i className={styles.ButtonText}>Go Home</i>
+        </button>
+      </a>
+    </div>
+  );
+};
